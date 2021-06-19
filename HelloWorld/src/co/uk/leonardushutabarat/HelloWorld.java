e6b727db8e48ddc623b988d7c3c107a93a8d5a31
leonardushutabarat <leonardus.hutabarat@gmail.com>
2021-06-19 10:20:05 +0100

    Initial commit

---
+++ HelloWorld/src/co/uk/leonardushutabarat/HelloWorld.java
@@ -0,0 +1,12 @@
+package co.uk.leonardushutabarat;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("My Name is Leo");
+
+	}
+
+}
